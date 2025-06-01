b92194fbd3109cce95e0d49799e9f7466aa3b3ff
Alicia Muñiz Jiménez <78161596+Alicia-MJ@users.noreply.github.com>
2025-05-31 20:05:09 -0600

    Update dyna_agents.py

--- neuronav/agents/dyna_agents.py
+++ neuronav/agents/dyna_agents.py
@@ -60,45 +60,6 @@ class DynaModule:
 
 
 
-
-class DynaSR_B(TDSR_B):
-    """
-    Dyna-enabled version of Temporal Difference Successor Representation algorithm.
-    """
-
-    def __init__(
-        self,
-        state_size: int,
-        action_size: int,
-        lr: float = 1e-1,
-        gamma: float = 0.99,
-        poltype: str = "softmax",
-        beta: float = 1e4,
-        epsilon: float = 1e-1,
-        w_value: float = 1.0,
-        num_recall:int = 5
-
-    ):
-        super(DynaSR_B, self).__init__(
-            state_size,
-            action_size,
-            lr=lr,
-            gamma=gamma,
-            poltype=poltype,
-            beta=beta,
-            epsilon=epsilon,
-            w_value=w_value,
-        )
-        self.num_recall = num_recall
-        self.dyna = DynaModule(state_size, self.num_recall)
-
-
-
-    def update(self, current_exp):
-        _ = super().update(current_exp)
-        self = self.dyna.update(self, current_exp)
-
-
 class DynaSR_A(TDSR_A):
     """
     Dyna-enabled version of Temporal Difference Successor Representation algorithm.
@@ -139,5 +100,44 @@ class DynaSR_A(TDSR_A):
 
 
 
+
+
+
+class DynaSR_B(TDSR_B):
+    """
+    Dyna-enabled version of Temporal Difference Successor Representation algorithm.
+    """
+
+    def __init__(
+        self,
+        state_size: int,
+        action_size: int,
+        lr: float = 1e-1,
+        gamma: float = 0.99,
+        poltype: str = "softmax",
+        beta: float = 1e4,
+        epsilon: float = 1e-1,
+        w_value: float = 1.0,
+        num_recall:int = 5
+
+    ):
+        super(DynaSR_B, self).__init__(
+            state_size,
+            action_size,
+            lr=lr,
+            gamma=gamma,
+            poltype=poltype,
+            beta=beta,
+            epsilon=epsilon,
+            w_value=w_value,
+        )
+        self.num_recall = num_recall
+        self.dyna = DynaModule(state_size, self.num_recall)
+
+
+
+    def update(self, current_exp):
+        _ = super().update(current_exp)
+        self = self.dyna.update(self, current_exp)
 
 
